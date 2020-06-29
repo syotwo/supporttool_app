328b22999ccfa5f77c4e03a782abc9682c350738
syotwo <44368100+syotwo@users.noreply.github.com>
2020-06-29 17:57:41 +0900

    item controller update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,6 +15,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = current_user.items.build(item_params)
+    #本来の記述である@item = current_user.items.build(params[:user])だとrails4から適応のstrong paramsでエラー
     
     if @item.save
       flash[:success] = 'tool を投稿しました。'
@@ -22,6 +23,19 @@ class ItemsController < ApplicationController
     else
       flash.now[:danger] = 'tool が投稿されませんでした'
       render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 
-  resources :items, only: [:index, :show, :new, :create]
+  resources :items
 end
