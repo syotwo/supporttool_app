b76d2bbcb21cf09ad63fcd21d1f2c2d0da9fc116
syotwo <44368100+syotwo@users.noreply.github.com>
2020-07-08 15:48:52 +0900

    user list一覧機能追加

--- app/controllers/item_lists_controller.rb
+++ app/controllers/item_lists_controller.rb
@@ -13,10 +13,25 @@ class ItemListsController < ApplicationController
   end
 
   def create
+    @item_list = current_user.item_lists.build(item_list_params)
+
+    if @item_list.save
+      redirect_to item_lists_path
+    else
+      render :new
+    end
   end
 
   def destroy
   end
-end
+
+  private
+
+  # Strong Parameter
+  def item_list_params
+    params.require(:item_list).permit(:list_name)
+  end
 
 # binding.pry
+end
+
