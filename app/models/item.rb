5f0949a69ed659a831ef0e8d9deb1d0f3b68e854
syotwo <44368100+syotwo@users.noreply.github.com>
2020-08-03 20:36:53 +0900

    [update]toppage配色デザイン変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :item_list
 
   validates :item_name, presence: true, length: { maximum: 150 }
   validates :img, presence: true
--- app/models/item_list.rb
+++ app/models/item_list.rb
@@ -1,10 +1,11 @@
 class ItemList < ApplicationRecord
   belongs_to :user
-
+  
   validates :list_name, presence: true, length: { maximum: 50 }
 
   mount_uploader :image, ItemListImgUploader
 
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
+  has_many :items
 end
