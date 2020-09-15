11a07b148058192c14ca29309fdd21abd3c3b8a4
syotwo <44368100+syotwo@users.noreply.github.com>
2020-09-15 15:36:51 +0900

    [update]toppageデザイン修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,10 @@
 class ToppagesController < ApplicationController
   
   def index
-     @item_lists = ItemList.all
+     @item_lists = ItemList.all.order(created_at: :desc)
+     @goodsleepsets = ItemList.where('list_name like ? or list_name like ?','%Sleep%','%ベッド%')
+     @mysets = ItemList.where(user_id: session[:user_id])
+     
+    #  binding.pry
   end
 end
