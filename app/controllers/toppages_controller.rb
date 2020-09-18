3df1b7b3ae4c69a1a5c50f5aeb70efe869e8d6f9
syotwo <44368100+syotwo@users.noreply.github.com>
2020-09-18 17:40:29 +0900

    [update]cardデザイン修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,9 @@ class ToppagesController < ApplicationController
   
   def index
      @item_lists = ItemList.all.order(created_at: :desc)
-     @goodsleepsets = ItemList.where('list_name like ? or list_name like ?','%Sleep%','%ベッド%')
-     @mysets = ItemList.where(user_id: session[:user_id])
+     @sleepsets = ItemList.where('list_name like ? or list_name like ?','%Sleep%','%ベッド%').order(created_at: :desc)
+     @mysets = ItemList.where(user_id: session[:user_id]).order(created_at: :desc)
+     @creativesets = ItemList.where('list_name like ? or list_name like ?','%Creative%','%クリエイティブ%').order(created_at: :desc)
      
     #  binding.pry
   end
