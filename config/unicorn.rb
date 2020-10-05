8d2e830141efffd8da0d6a7fcffc0f1a674ad1eb
syotwo <44368100+syotwo@users.noreply.github.com>
2020-10-05 13:41:16 +0900

    [update]unicornファイル場所変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,7 @@ app_path = File.expand_path('../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する 
 worker_processes 1
+
 #アプリケーションの設置されているディレクトリを指定 
 working_directory app_path
 
@@ -11,6 +12,7 @@ pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定 
 listen 3000
+
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
@@ -21,32 +23,29 @@ stdout_path "#{app_path}/log/unicorn.stdout.log"
 timeout 60
 
 #以下は応用的な設定なので説明は割愛 
+
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
 check_client_connection false
 
 run_once = true
 
-before_fork do |server, worker| 
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
+before_fork do |server, worker| defined?(ActiveRecord::Base) &&
+ActiveRecord::Base.connection.disconnect!
 
-  if run_once
-  run_once = false # prevent from firing again
-  end
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => 
-      e logger.error e
-    end 
-  end
+if run_once
+run_once = false # prevent from firing again
+end
+old_pid = "#{server.config[:pid]}.oldbin"
+if File.exist?(old_pid) && server.pid != old_pid
+begin
+sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+Process.kill(sig, File.read(old_pid).to_i)
+rescue Errno::ENOENT, Errno::ESRCH => e logger.error e
+end end
 end
 
 after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
