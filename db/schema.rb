e0f48311ca7ad975b69e1fb04cf8c23ba95ebec2
syotwo <44368100+syotwo@users.noreply.github.com>
2020-10-08 19:59:23 +0900

    [update]カードデザイン修正

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_032537) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_list_id"], name: "index_favorites_on_item_list_id"
+    t.index ["user_id", "item_list_id"], name: "index_favorites_on_user_id_and_item_list_id", unique: true
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
