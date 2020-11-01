b167017fca73f2e54278527dc17239305fb6e0f3
syotwo <44368100+syotwo@users.noreply.github.com>
2020-11-01 19:36:36 +0900

    [update]toppageデザイン修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,6 +2,9 @@ class ToppagesController < ApplicationController
   
   def index
      @item_lists = ItemList.all.order(created_at: :desc)
+     @brainsets = ItemList.where('list_name like ? or list_name like ?','%brain%','%脳%')
+     @bodysets = ItemList.where('list_name like ? or list_name like ?','%body%','%体%')
+     @mindsets = ItemList.where('list_name like ? or list_name like ?','%mid%','%心%')
      @sleepsets = ItemList.where('list_name like ? or list_name like ?','%Sleep%','%ベッド%')
      @mysets = ItemList.where(user_id: session[:user_id])
      @creativesets = ItemList.where('list_name like ? or list_name like ?','%Creative%','%クリエイティブ%')
