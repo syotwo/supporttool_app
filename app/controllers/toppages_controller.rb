0a92780a365d506a10cc9aad81cc32b969fbec35
syotwo <44368100+syotwo@users.noreply.github.com>
2020-11-30 18:10:30 +0900

    [update]toppageアイテムカテゴリ修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,13 +1,12 @@
 class ToppagesController < ApplicationController
   
   def index
-     @item_lists = ItemList.all.order(created_at: :desc)
-     @brainkit = ItemList.where('list_name like ? or list_name like ?','%brain%','%脳%')
-     @sleepkit = ItemList.where('list_name like ? or list_name like ?','%Sleep%','%ベッド%')
-     @mykit = ItemList.where(user_id: session[:user_id])
-     @creativekit = ItemList.where('list_name like ? or list_name like ?','%Creative%','%クリエイティブ%')
-     @creativekit = ItemList.where('list_name like ? or list_name like ?','%Creative%','%クリエイティブ%')
-     
+     @item_lists = ItemList.all.recent
+     @digitalkit = ItemList.digital_category.recent
+     @sleepkit = ItemList.sleep_category.recent
+     @mykit = ItemList.where(user_id: session[:user_id]).recent
+     @creativekit = ItemList.creative_category.recent
+     @dailykit = ItemList.daily_category.recent
     #  binding.pry
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :recent, -> { order(created_at: :desc) }
+  scope :digital_category, -> { where('list_name like ? or list_name like ?','%brain%','%脳%') }
+  scope :sleep_category, -> { where('list_name like ? or list_name like ?','%Sleep%','%ベッド%') }
+  scope :creative_category, -> { where('list_name like ? or list_name like ?','%Creative%','%クリエイティブ%') }
+  scope :daily_category, -> { where('list_name like ? or list_name like ?','%daily%','%ケア%') }  
+  
+  
 end
