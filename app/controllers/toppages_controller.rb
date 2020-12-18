b20d063f9d1db5144241d2e08fa19c8edfde959a
syotwo <44368100+syotwo@users.noreply.github.com>
2020-12-18 17:33:13 +0900

    [update]toppage、ボックスデザイン変更

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,16 +1,6 @@
 class ToppagesController < ApplicationController
   
   def index
-     @item_lists = ItemList.all.recent
-     @digitalkit = ItemList.digital_category.recent
-     @sleepkit = ItemList.sleep_category.recent
-     @mykit = ItemList.where(user_id: session[:user_id]).recent
-     @creativekit = ItemList.creative_category.recent
-     @dailykit = ItemList.daily_category.recent
-     @reducestresskit = ItemList.reducestress_category
-     @focuskit = ItemList.focus_category
-
-     
-    #  binding.pry
+    @items = Item.all
   end
 end
