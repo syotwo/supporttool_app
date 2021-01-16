b7a3f4cbf745aaaae601a659d1c29b3748912873
syotwo <44368100+syotwo@users.noreply.github.com>
2021-01-16 17:59:47 +0900

    [update]サービス名フォント変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require('jquery')
 require("chartkick") 
 require("chart.js") 
 
-require ('slick.js')
+require ('user/sample')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
