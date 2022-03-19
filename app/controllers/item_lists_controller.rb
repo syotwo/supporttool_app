ecd95d6513c7b39b57f00e2fba457a71b1a146fc
syotwo <44368100+syotwo@users.noreply.github.com>
2022-03-19 16:41:35 +0900

    [destroy]カテゴリーコントロラー削除

--- app/controllers/item_lists_controller.rb
+++ app/controllers/item_lists_controller.rb
@@ -56,7 +56,7 @@ class ItemListsController < ApplicationController
 
   # Strong Parameter
   def item_list_params
-    params.require(:item_list).permit(:list_name, :image, :list_description, :list_item_type)
+    params.require(:item_list).permit(:list_name, :image, :list_description, :list_item_type, :category_id)
   end
 
   def correct_user
