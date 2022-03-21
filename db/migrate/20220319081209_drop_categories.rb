16b44eb09a26e90fb683e3b118e322c80f89243d
syotwo <44368100+syotwo@users.noreply.github.com>
2022-03-21 15:29:59 +0900

    [destroy]カテゴリーテーブル再作成-マイグレーションファイルの削除（カテゴリー削除）

--- db/migrate/20220319081209_drop_categories.rb
+++
@@ -1,7 +0,0 @@
-class DropCategories < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :categories do |t|
-      t.string :name, null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_081209) do
+ActiveRecord::Schema.define(version: 2022_03_19_075754) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+  end
 
   create_table "emotions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "sheet"
