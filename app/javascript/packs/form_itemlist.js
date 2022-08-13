2106d508342bc0137d722d79890485bc7e018daa
syotwo <44368100+syotwo@users.noreply.github.com>
2022-08-13 15:38:22 +0900

    [modify]フォームをフロント実装完（jqueryで）--ステップバーの色、ホームの横スライド機能を実装完

--- app/javascript/packs/form_itemlist.js
+++ app/javascript/packs/form_itemlist.js
@@ -1,12 +1,98 @@
-document.addEventListener('turbolinks:load', () => {
-  alert("test")
-})
+// document.addEventListener('turbolinks:load', () => {
+//   alert("test")
+// })
 
-var Form1 = document.getElementById("Form1");
-var Form2 = document.getElementById("Form2");
+// var Form1 = document.getElementById("Form1");
+// var Form2 = document.getElementById("Form2");
 
-var Next1 = document.getElementById("Next1");
+// var Next1 = document.getElementById("Next1");
 
-Next1.onclick = function() {
-  Form1.style.display = "none";
-}
+// Next1.onclick = function() {
+//   Form1.style.display = "none";
+// }
+
+
+// jQuery
+
+// $(function(){
+//   $('#Next1').on('click', function() {
+//     $('#Form1').hide();
+//     $('#Form2').show();
+//   });
+// });
+
+$(function() {
+  $('#Next1').click(function() {
+    $('#Form1').animate({
+      left: '-1000px',
+    });
+    $('#Form2').animate({
+      left: '0px',
+    });
+    $('.step_col1').css('background-color','orange');
+  });
+});
+
+$(function(){
+  $('#Back1').click(function() {
+    $('#Form1').animate({
+      left: '0px',
+    });
+    $('#Form2').animate({
+      left: '1000px',
+    });
+    $('.step_col1').css('background-color','white');
+  });
+});
+
+
+
+$(function() {
+  $('#Next2').click(function() {
+    $('#Form2').animate({
+      left: '-1000px',
+    });
+    $('#Form3').animate({
+      left: '1000px',
+    });
+    $('.step_col2').css('background-color','orange');
+  });
+});
+
+$(function(){
+  $('#Back2').click(function() {
+    $('#Form2').animate({
+      left: '0px',
+    });
+    $('#Form3').animate({
+      left: '1000px',
+    });
+    $('.step_col2').css('background-color','white');
+  });
+});
+
+
+
+$(function() {
+  $('#Next3').click(function() {
+    $('#Form3').animate({
+      left: '0px',
+    });
+    $('#Form4').animate({
+      left: '1000px',
+    });
+    $('.step_col3').css('background-color','orange');
+  });
+});
+
+$(function(){
+  $('#Back3').click(function() {
+    $('#Form3').animate({
+      left: '1000px',
+    });
+    $('#Form4').animate({
+      left: '1000px',
+    });
+    $('.step_col3').css('background-color','white');
+  });
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+// 以下Webpackの設定ファイルでjQueryを管理下として認定します。
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+// ここまで
+
 module.exports = environment
